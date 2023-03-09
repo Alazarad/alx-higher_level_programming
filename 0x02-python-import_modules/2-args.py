079f7c41cae64058ed7ce60f20db9b567e159bc2
Alazarad <demissiealazarad@gmail.com>
2023-03-09 01:12:35 -0800

    correction of task 2 of import and modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-import sys
-count = len(sys.argv) - 1
-if count == 1:
-    print("{} argument:".format(count))
-elif  count == 0:
-    print("{} arguments.".format(count))
-else:
-    print("{} arguments:".format(count))
-for i in range(count):
-    print("{}: {}".format((i + 1), sys.argv[i + 1]))
+if __name__ == "__main__":
+    import sys
+    count = len(sys.argv) - 1
+    if count == 1:
+        print("{} argument:".format(count))
+    elif  count == 0:
+        print("{} arguments.".format(count))
+    else:
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format((i + 1), sys.argv[i + 1]))
