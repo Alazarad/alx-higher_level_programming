203751dd6611592dd42730dcee5a4b947c2ea903
Alazarad <demissiealazarad@gmail.com>
2023-03-13 01:12:02 -0700

    correction of task 5 of data structure

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = my_string.translate(ord('c'):None)
-    newstring = new_string.translate(ord('C'):None)
-    return(newstring)
+    new_string = my_string[:]
+    x = 0
+    for i in my_string:
+        if my_string[i] == 'c' or my_string[i] == 'C':
+            new_string = new_string[:(i - x)] + my_string[(i + 1)]
+            x += 1
+        return(new_string)
