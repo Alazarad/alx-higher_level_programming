b378ad74f63731b1b971fe06f8d87c437942fbe2
Alazarad <demissiealazarad@gmail.com>
2023-03-15 05:37:34 -0700

    correction of task 2 of more data structures

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -2,6 +2,6 @@
 def uniq_add(my_list=[]):
     new_list = set(my_list)
     result = 0
-    for i in range(len(new_list) + 1):
+    for i in new_list:
         result = result + i
     return(result)
