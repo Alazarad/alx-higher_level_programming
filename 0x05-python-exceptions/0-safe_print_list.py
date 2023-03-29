ffc700937f4e89defe311b3e97e0c39e3436d44e
Alazarad <demissiealazarad@gmail.com>
2023-03-29 05:36:05 -0700

    correction fo task 2 of python exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     cou = 0
     for i in range(x):
         try:
-            print("P{}".format(my_list[i]), end="")
+            print("{}".format(my_list[i]), end="")
             cou += 1
         except IndexError:
             break
